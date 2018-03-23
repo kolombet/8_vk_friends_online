949c3a3134b11ac98ef4472eda793204e82bb927
anonra <anonrab@ya.ru>
2018-03-23 08:09:52 +0000

    friends online script

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,79 @@
 import vk
+import argparse
+import sys
+from getpass import getpass
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
 
-
-def get_user_login():
-    pass
-
-
-def get_user_password():
-    pass
-
-
-def get_online_friends(login, password):
+def get_api(app_id, user_login, user_password, api_version):
     session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
+        app_id=app_id,
+        user_login=user_login,
+        user_password=user_password,
     )
-    api = vk.API(session)
-    # например, api.friends.get()
+    api_version = "5.73"
+    return vk.API(session, v=api_version)
+
+
+def get_frineds(api):
+    return api.friends.get(fields="domain")["items"]
+
+
+def get_online_friends(api):
+    friends = get_frineds(api)
+    friends_online = [friend for friend in friends if friend["online"] == 1]
+    return friends_online
 
 
 def output_friends_to_console(friends_online):
+    for friend in friends_online:
+        print("{} {}".format(friend["first_name"], friend["last_name"]));
     pass
 
-if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
-    friends_online = get_online_friends(login, password)
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-l",
+        "--login",
+        dest="login",
+        help="vk.com login"
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        dest="version",
+        help="vk api version"
+    )
+    parser.add_argument(
+        "-a",
+        "--appid",
+        dest="app_id",
+        help="vk app id"
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = get_args()
+    
+    login = args.login
+    if not login:
+        sys.exit("please specify login")
+        
+    app_id = args.app_id
+    if not app_id:
+        sys.exit("please specify app id")
+        
+    default_api_version = "5.73"
+    version = args.version
+    if not version:
+        version = default_api_version 
+        
+    password = getpass("enter password:")
+    if not password or password.isspace():
+        sys.exit("please enter password")
+    api = get_api(app_id, login, password, version)
+    friends_online = get_online_friends(api)
+    print("friends online:")
     output_friends_to_console(friends_online)
